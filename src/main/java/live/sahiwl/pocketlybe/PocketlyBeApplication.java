dbf76c2a147c8ae57b92955e17e35232a567e3ea
sahiwl <piyushkrray1@gmail.com>
2025-09-10 17:22:14 +0530

    init

---
+++ src/main/java/live/sahiwl/pocketlybe/PocketlyBeApplication.java
@@ -0,0 +1,13 @@
+package live.sahiwl.pocketlybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketlyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocketlyBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/live/sahiwl/pocketlybe/PocketlyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package live.sahiwl.pocketlybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketlyBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
